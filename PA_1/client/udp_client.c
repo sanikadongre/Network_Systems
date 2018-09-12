11ecfb0885dc11592017e183e0fda614ddb62ea4
Sanika Dongre <sado9543@colorado.edu>
2018-09-11 19:53:51 -0600

    Update udp_client.c

--- PA_1/client/udp_client.c
+++ PA_1/client/udp_client.c
@@ -1,4 +1,4 @@
-// Server side implementation of UDP client-server model 
+// Client side implementation of UDP client-server model 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -15,41 +15,35 @@
 int main() { 
 	int sockfd; 
 	char buffer[MAXLINE]; 
-	char *hello = "Hello from server"; 
-	struct sockaddr_in servaddr, cliaddr; 
-	
+	char *hello = "Hello from client"; 
+	struct sockaddr_in	 servaddr; 
+
 	// Creating socket file descriptor 
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
 		perror("socket creation failed"); 
 		exit(EXIT_FAILURE); 
 	} 
-	
+
 	memset(&servaddr, 0, sizeof(servaddr)); 
-	memset(&cliaddr, 0, sizeof(cliaddr)); 
 	
 	// Filling server information 
-	servaddr.sin_family = AF_INET; // IPv4 
-	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
 	
-	// Bind the socket with the server address 
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-			sizeof(servaddr)) < 0 ) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
+	int n, len; 
 	
-	int len, n; 
+	sendto(sockfd, (const char *)hello, strlen(hello), 
+		MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+			sizeof(servaddr)); 
+	printf("Hello message sent.\n"); 
+		
 	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-				MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+				MSG_WAITALL, (struct sockaddr *) &servaddr, 
 				&len); 
 	buffer[n] = '\0'; 
-	printf("Client : %s\n", buffer); 
-	sendto(sockfd, (const char *)hello, strlen(hello), 
-		MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
-			len); 
-	printf("Hello message sent.\n"); 
-	
+	printf("Server : %s\n", buffer); 
+
+	close(sockfd); 
 	return 0; 
 } 
