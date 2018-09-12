47e7f3f5f03ef44f6bafb45572645c582dd3de32
Sanika Dongre <sado9543@colorado.edu>
2018-09-11 19:51:49 -0600

    Update udp_client.c

--- PA_1/client/udp_client.c
+++ PA_1/client/udp_client.c
@@ -1,131 +1,55 @@
-/* 
- * udpclient.c - A simple UDP client
- * usage: udpclient <host> <port>
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-#include <stdint.h>
+// Server side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
 
-#define BUFSIZE 1024
+#define PORT	 8080 
+#define MAXLINE 1024 
 
-typedef struct
-{
-	uint16_t index_packet;
-	uint16_t length;
-	uint8_t pack_size[BUFSIZE];
-}Packet_details;
-
-void encryptdata(int buf_size, uint8_t* buffer, uint8_t* encrypteddata)
-{
-	uint8_t encryptedkey[8] = {'A','B','C','D','1', '2', '3', '4'};
-	int key_index=0;
-	for(int i=0;i<buf_size;i++)
-	{
-		encrypteddata[i] = buffer[i] + encryptedkey[key_index];
-		key_index++;
-		if(key_index==8)
-		{
-			key_index=0;
-		}
-	}
-}
-
-void decryptdata(int buf_size, uint8_t* buffer, uint8_t* decrypteddata){
-	uint8_t encryptedkey[8] = {'A','B','C','D','1','2','3', '4'};
-	int key_index=0;
-	for(int i=0;i<datasize;i++)
-	{
-		decryptedbuff[i] = buffer[i] - encryptedkey[key_index] ;
-		key_index++;
-		if(key_index==8)
-		{
-			key_index=0;
-		}
-	}
-}
-
-
-void get(uint8_t* file_name, int sock, struct sockaddr_in remote)
-{
-	int file_receive_check = 0, send_data;
-	uint8_t message[BUFSIZE] = " ", size_buf[BUFSIZE] = " ";
-	bzero(message, sizeof(message));
-	file_receive_check = recvfrom(sock, message, sizeof(message), 0 , NULL, NULL);
-	bzero(size_buf, sizeof(size_buf));
-	if(strcmp(message, "File found") != 0)
-	{
-		printf("The file is not found\n");
-	}
-	else
-	{
-		send_data = sendto(sock, size_buf, strlen(size_buf), 0, (struct
-		
-		
+// Driver code 
+int main() { 
+	int sockfd; 
+	char buffer[MAXLINE]; 
+	char *hello = "Hello from server"; 
+	struct sockaddr_in servaddr, cliaddr; 
 	
+	// Creating socket file descriptor 
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
 	
-/* 
- * error - wrapper for perror
- */
-void error(char *msg) {
-    perror(msg);
-    exit(0);
-}
-int main(int argc, char **argv) {
-    int sockfd, portno, n;
-    int serverlen;
-    struct sockaddr_in serveraddr;
-    struct hostent *server;
-    char *hostname;
-    char buf[BUFSIZE];
-
-    /* check command line arguments */
-    if (argc != 3) {
-       fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
-       exit(0);
-    }
-    hostname = argv[1];
-    portno = atoi(argv[2]);
-
-    /* socket: create the socket */
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-
-    /* gethostbyname: get the server's DNS entry */
-    server = gethostbyname(hostname);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
-        exit(0);
-    }
-
-    /* build the server's Internet address */
-    bzero((char *) &serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-    serveraddr.sin_port = htons(portno);
-
-    /* get a message from the user */
-    bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
-
-    /* send the message to the server */
-    serverlen = sizeof(serveraddr);
-    n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
-    if (n < 0) 
-      error("ERROR in sendto");
-    
-    /* print the server's reply */
-    n = recvfrom(sockfd, buf, strlen(buf), 0, &serveraddr, &serverlen);
-    if (n < 0) 
-      error("ERROR in recvfrom");
-    printf("Echo from server: %s", buf);
-    return 0;
-}
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	memset(&cliaddr, 0, sizeof(cliaddr)); 
+	
+	// Filling server information 
+	servaddr.sin_family = AF_INET; // IPv4 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	servaddr.sin_port = htons(PORT); 
+	
+	// Bind the socket with the server address 
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
+			sizeof(servaddr)) < 0 ) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	int len, n; 
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+				MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+				&len); 
+	buffer[n] = '\0'; 
+	printf("Client : %s\n", buffer); 
+	sendto(sockfd, (const char *)hello, strlen(hello), 
+		MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+			len); 
+	printf("Hello message sent.\n"); 
+	
+	return 0; 
+} 
