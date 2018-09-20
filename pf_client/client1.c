0eddcc1c048acaed057d5b36f3017c4a4bd88f48
sanikadongre <sanikadongre>
2018-09-19 19:13:44 -0600

    adding

--- pf_client/client1.c
+++ pf_client/client1.c
@@ -10,9 +10,25 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <stdint.h>
+#include <time.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <memory.h>
+#include <errno.h>
+#include <dirent.h>
+#include <string.h>
 
 #define BUFSIZE 1024
 
+typedef struct
+{
+	uint8_t packet_descp[BUFSIZE];
+	uint32_t packet_len;
+	uint32_t packet_index;
+}Packet_Details;
+
+struct timeval time_vals, time_val1, time_val2, time_done;
 /* 
  * error - wrapper for perror
  */
@@ -22,12 +38,18 @@ void error(char *msg) {
 }
 
 int main(int argc, char **argv) {
-    int sockfd, portno, n;
+    int sockfd, portno, bytestot, bytestot1, n;
     int serverlen;
     struct sockaddr_in serveraddr;
+    uint8_t hash_buf[100], val[BUFSIZE], cmd[70], fname1[70], cmd_out_exit;
+    uint8_t* fname;
+    uint8_t* name_cmd;
     struct hostent *server;
     char *hostname;
     char buf[BUFSIZE];
+    bzero(cmd, sizeof(cmd));
+    bzero(fname1, sizeof(fname1));
+    bzero(val, sizeof(val));
 
     /* check command line arguments */
     if (argc != 3) {
@@ -40,7 +62,9 @@ int main(int argc, char **argv) {
     /* socket: create the socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) 
+    {
         error("ERROR opening socket");
+    }
 
     /* gethostbyname: get the server's DNS entry */
     server = gethostbyname(hostname);
@@ -58,19 +82,28 @@ int main(int argc, char **argv) {
 
     /* get a message from the user */
     bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
-
-    /* send the message to the server */
-    serverlen = sizeof(serveraddr);
-    n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr*)&serveraddr, serverlen);
-    if (n < 0) 
-      error("ERROR in sendto");
-    
-    /* print the server's reply */
-    n = recvfrom(sockfd, buf, strlen(buf), 0, (struct sockaddr*)&serveraddr, &serverlen);
-    if (n < 0) 
-      error("ERROR in recvfrom");
-    printf("Echo from server: %s", buf);
-    return 0;
+    while(1)
+  	{
+		    time_val1.tv_sec = 0;
+		    time_val1.tv_usec = 0;
+		    if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &time_val1, sizeof(time_val1)) < 0)
+		   {
+			perror("Error\n");
+		    }
+		    printf("Please enter msg: ");
+		    fgets(buf, BUFSIZE, stdin);
+		
+		    /* send the message to the server */
+		    serverlen = sizeof(serveraddr);
+		    n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr*)&serveraddr, serverlen);
+		    if (n < 0) 
+		      error("ERROR in sendto");
+		    
+		    /* print the server's reply */
+		    n = recvfrom(sockfd, buf, strlen(buf), 0, (struct sockaddr*)&serveraddr, &serverlen);
+		    if (n < 0) 
+		      error("ERROR in recvfrom");
+	    printf("Echo from server: %s", buf);
+	}
+    	    return 0;
 }
