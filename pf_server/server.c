cf0c9b3c125207f3ccbb5b2a26e05a24b438a216
sanikadongre <sanikadongre>
2018-09-19 19:50:37 -0600

    addition

--- pf_server/server.c
+++ pf_server/server.c
@@ -12,8 +12,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <memory.h>
+#include <string.h>
+#include <server.h>
+#include <stdint.h>
+#include <sys/time.h>
+
 
 #define BUFSIZE 1024
+struct timeval time_vals, time_val1, time_val2, time_done;
+
+typedef struct
+{
+	uint32_t packet_index;
+	uint8_t packet_descp[BUFSIZE];
+	uint32_t packet_len;
+}Packet_Details;
 
 /*
  * error - wrapper for perror
@@ -23,18 +42,19 @@ void error(char *msg) {
   exit(1);
 }
 
-int main(int argc, char **argv) {
-  int sockfd; /* socket */
-  int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
-  struct sockaddr_in serveraddr; /* server's addr */
-  struct sockaddr_in clientaddr; /* client addr */
+int main(int argc, char **argv)
+ {
+  int sockfd, portno, clientlen, optval, n, client_socket, bytestot = 0, file_del, exit_recv, bytestot1 = 0; /* socket */
+  struct sockaddr_in serveraddr, clientaddr; /* server's addr */
   struct hostent *hostp; /* client host info */
   char buf[BUFSIZE]; /* message buf */
   char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
-
+  uint8_t* fname;
+  uint8_t* name_cmd;
+  uint8_t cmd_out_exit, val[BUFSIZE] = "The server is exiting", cmd[70], fname1[70], check = 1;
+  bzero(cmd, sizeof(cmd));
+  bzero(fname1, sizeof(fname1));
+  bzero(val, sizeof(val));
   /* 
    * check command line arguments 
    */
@@ -73,13 +93,15 @@ int main(int argc, char **argv) {
    */
   if (bind(sockfd, (struct sockaddr *) &serveraddr, 
 	   sizeof(serveraddr)) < 0) 
-    error("ERROR on binding");
+   {
+    	error("ERROR on binding");
+   }
 
   /* 
    * main loop: wait for a datagram, then echo it
    */
   clientlen = sizeof(clientaddr);
-  while (1) {
+  while (check) {
 
     /*
      * recvfrom: receive a UDP datagram from a client
