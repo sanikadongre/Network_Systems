c04f4a866fadfacfaa9f1cf64c22bcee1aac1098
Sanika Dongre <sado9543@colorado.edu>
2018-09-20 16:48:06 -0600

    Create client.h

---
+++ client.h
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+#include <fcntl.h>
+
+typedef enum {SUCCESS=0,FAILURE=1} check_t ;
+
+#define MAXLIST 20
+
+#endif
