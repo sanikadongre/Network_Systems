4d28954b1cd89550e9a0198955f8f187d8a7dbdf
Sanika Dongre <sado9543@colorado.edu>
2018-09-23 16:48:44 -0600

    Update client1.c

--- netsys_t/client/client1.c
+++ netsys_t/client/client1.c
@@ -53,23 +53,23 @@ void data_decryption(char *buffer, int data_len, char key1[], char key2[]){
 /***** MAIN FUNCTION - ENTRY POINT *****/
 int main (int argc, char * argv[])
 {
-		 			// to set stdout to not buffer printf -> Refernce: stackoverflow.com
-	int nbytes;                             // number of bytes to be sent or received
-	int length;															// length of the sockaddr_in structure
-	int sockfd;                               // this will be our socket
-	char command[100];											// to store user input
-	char buffer[BUFSIZE];								// buffer for storing the data to be sent/recieved
-	struct sockaddr_in server, remote;      //"Internet socket address structure"
-	struct hostent *server_hp;							// to represent entry in host database
-	char *cname;														// to extract the command from user input
-	char *filename;													// to store the filename(in any) from user input
-	FILE *fp;																// declaring file pointer to perform file read/write
-	struct_pckt* c_pckt = malloc(sizeof(struct_pckt)); 				// defining packet structure for client packet
-	struct_pckt* s_pckt = malloc(sizeof(struct_pckt)); 				// defining packet structure for server packet
-	struct timeval timeout; 								// defining timeout structure
-	int read_length;												// to store the length of the read data
-	char key1[39] = "MyMethodOfEncrptingTheDataIIntendToSend";// Data encryption key-1
-	char key2[59] = "ThisIsMySecondKeyForMyMethodOfEncrptingTheDataIIntendToSend";// Data encryption key-2
+		 			
+	int nbytes;                            
+	int length;															
+	int sockfd;                              
+	char command[100];											
+	char buffer[BUFSIZE];								
+	struct sockaddr_in server, remote;     
+	struct hostent *server_hp;							
+	char *cname;														
+	char *filename;													
+	FILE *fp;															
+	struct_pckt* c_pckt = malloc(sizeof(struct_pckt)); 				
+	struct_pckt* s_pckt = malloc(sizeof(struct_pckt)); 				
+	struct timeval timeout; 								
+	int read_length;												
+	char key1[39] = "MyMethodOfEncrptingTheDataIIntendToSend";
+	char key2[59] = "ThisIsMySecondKeyForMyMethodOfEncrptingTheDataIIntendToSend";
 
 /***** Check for required number of command line arguments
 to connect to server *****/
@@ -110,54 +110,39 @@ to connect to server *****/
 	length = sizeof(struct sockaddr_in);
 
 	while(1){
-		/* Initializing timeout struct */
+		
 		timeout.tv_sec = 0;
 		timeout.tv_usec = 0;
 		setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
 		bzero(buffer, sizeof(buffer)); // clear buffer
 
-		/* Display the availble commands for the user */
+		
 		printf("\n\tEnter Command as under: \n");
 		printf("\t1. get <filename> \n" "\t2. put <filename>\n" "\t3. delete <filename>\n"
 		"\t4. ls\n" "\t5. exit \n" );
-		gets(command);	// store the command entered by the user
-
-		/* Send the command to the server */
-		nbytes = sendto(sockfd, command, sizeof(command) , 0, (struct sockaddr *)&server, length);
-
-
-		/***** Split the command from the user to get the command and filename if any
-		 Refernce: http://www.geeksforgeeks.org/how-to-split-a-string-in-cc-python-and-java/
-		 *****/
+		gets(command);	
+		nbytes = sendto(sockfd, command, sizeof(command) , 0, (struct sockaddr *)&server, length)
 		cname = strdup(command);
 		strtok(cname, " ");
 		filename = strtok(NULL, " ");
 		printf("Filename: %s\n", filename);
-
-		/***** Condition: To request file from the server
-		Given a valid file name by the user, which is available
-		at the server *****/
 		if(!strcmp(cname, "get")){
 			printf("Get File: %s from the server.\n", filename);
-			int exp_index=1; 	// Initializing the expected packet index with 1
+			int exp_index=1; 	
 
 			nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &remote, &length);
 			if(!strcmp(buffer, "Error")){
 				printf("File does not exist on the server. \n");
 				continue;
 			}
-
-			//Starting the loop for continous receive of data from the server
 			do{
 				bzero(s_pckt->data_buff, sizeof(s_pckt->data_buff));
 				nbytes = recvfrom(sockfd, (struct_pckt*) s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &remote, &length);
 
 				printf("Packet Size from the server: %d \n",nbytes);
 
-				data_decryption(s_pckt->data_buff, s_pckt->len_data, key1, key2);// Decrypting the encoded data from server
-
-				//Packet Authentication
+				data_decryption(s_pckt->data_buff, s_pckt->len_data, key1, key2);
 				if(s_pckt->pckt_index == exp_index){
 					printf("Writing to the file\n" );
 					FILE *fp;
@@ -167,43 +152,30 @@ to connect to server *****/
 					fclose(fp);
 
 					c_pckt->pckt_ack=exp_index;
-
-					//Sending Acknowledgement if the expected packet is recieved
 					nbytes = sendto(sockfd, (struct_pckt*) c_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&server, length);
 					printf("Packet Size being sent to server: %d and ACK sent: %d\n",nbytes, c_pckt->pckt_ack);
 					exp_index++;
 				}
 				else{
 					c_pckt->pckt_ack=s_pckt->pckt_index;
-					//Sending Acknowledgement if the expected packet not received
 					nbytes = sendto(sockfd, (struct_pckt*)c_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&server, length);
 					printf("Packet Size being sent to server: %d and ACK sent: %d\n",nbytes, c_pckt->pckt_ack);
 				}
 				if(s_pckt->len_data != BUFSIZE){
 					break;
 				}
-			}while(1); // check for remaining data to be recieved
-
-			// Clearing the memory for the packet structures
+			}while(1); 
+			
 			memset(c_pckt, 0, sizeof(struct_pckt));
 			memset(s_pckt, 0, sizeof(struct_pckt));
 		}
 
-		/***** Condition: To send a file from the client to the server
-		Given a valid file name by the user, which is available
-		at the client *****/
 		else if(!strcmp(cname, "put")){
 			printf("Put File: \"%s\" on the server.\n", filename);
-
-			//clear memory for packet structres
 			memset(c_pckt, 0, sizeof(struct_pckt));
 			memset(s_pckt, 0, sizeof(struct_pckt));
-
-			//Initializing packet structure variables
 			c_pckt->pckt_index = 1;
 			s_pckt->pckt_ack = 0;
-
-			// Setting timeout
 			timeout.tv_sec = 0;
 			timeout.tv_usec = 300000;
 			setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
@@ -221,8 +193,6 @@ to connect to server *****/
 				char msg[] = "Success";
 				nbytes = sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&server, length);
 			}
-
-			/* Loop for continous sending of valid packets */
 			do{
 				bzero(c_pckt->data_buff, sizeof(c_pckt->data_buff));
 				read_length = fread(c_pckt->data_buff, 1, BUFSIZE , fp);
@@ -234,8 +204,6 @@ to connect to server *****/
 
 				bzero(s_pckt->data_buff, sizeof(s_pckt->data_buff));
 				nbytes = recvfrom(sockfd, (struct_pckt*) s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&remote, &length);
-
-				// Check for Packet-loss or acknowledgement loss
 				if(nbytes < 0){
 					printf("---------------------Timeout--------------------------\n");
 					fseek(fp, (-1)*read_length, SEEK_CUR);
@@ -243,7 +211,6 @@ to connect to server *****/
 				}
 				else{
 					printf("Packet Size from server: %d and ack_index from server: %d \n", nbytes, s_pckt->pckt_ack);
-					// Check for acknowledgement of the last packet sent
 					if(s_pckt->pckt_ack == c_pckt->pckt_index){
 						c_pckt->pckt_index++;
 					}
@@ -255,23 +222,14 @@ to connect to server *****/
 					break;
 				}
 			}while(1);
-
-			// Clear memory for packet structures
 			memset(c_pckt, 0, sizeof(struct_pckt));
 			memset(s_pckt, 0, sizeof(struct_pckt));
 		}
-
-		/***** Condition: To delete a file from the server
-		Given a valid file name by the user, which is available
-		at the server *****/
 		else if(!strcmp(cname, "delete")){
 			// Recieving acknowledgement whether the file is deleted.
 			nbytes = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&remote, &length);
 			printf(" %s \n", buffer );
 		}
-
-		/***** Condition: To list all the files available at the server
-		Reference: http://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/ *****/
 		else if(!strcmp(cname, "ls")){
 			printf("\nListing Files from the server.\n");
 			bzero(buffer, sizeof(buffer));
@@ -279,20 +237,16 @@ to connect to server *****/
 			printf("%s\n", buffer ); //Printing data recieved from the buffer
 		}
 
-		/***** Condition: To Request the server to gracefully release the
-		connection *****/
 		else if(!strcmp(cname, "exit")){
 			printf("Request server to release the connection.\n");
 			nbytes = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&remote, &length);
 			printf(" %s \n", buffer );
 		}
 
-		/****** Condition: If the Command entered
-		by the user is not supported *****/
 		else{
 			printf("Unsupported Command\n");
 		}
 
-		bzero(buffer,sizeof(buffer)); //clearing the buffer
+		bzero(buffer,sizeof(buffer)); 
 	}
 }
