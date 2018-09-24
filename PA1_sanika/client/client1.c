5f33108ab3813218f97a6b0d81beff334b14c231
Sanika Dongre <sado9543@colorado.edu>
2018-09-23 23:39:19 -0600

    Update client1.c

--- PA1_sanika/client/client1.c
+++ PA1_sanika/client/client1.c
@@ -1,4 +1,6 @@
-
+/***************************
+*Reference professors code and few websites for file related commands, listing files, string split
+*************************/
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -24,7 +26,7 @@ void error(char *msg) {
     perror(msg);
     exit(0);
 }
-
+/*Data encryption for relaibility*/
 void encode(uint8_t *temp_buf, int size_len, uint8_t key)
 {
 	for(int i=0; i<size_len; i++)
@@ -33,7 +35,7 @@ void encode(uint8_t *temp_buf, int size_len, uint8_t key)
 	}
 }
 
-
+/*Structure for Packet details*/
 typedef struct{
 	int pckt_index;						
 	int pckt_ack;														
@@ -58,9 +60,6 @@ int main (int argc, char * argv[])
         bzero(val, sizeof(val));
 	Packet_Details* a = malloc(sizeof(Packet_Details));
 	Packet_Details* s = malloc(sizeof(Packet_Details));
-
-	
-
 	if (argc < 3)
 	{
 		printf("USAGE:  <server_ip> <server_port>\n");
@@ -69,11 +68,11 @@ int main (int argc, char * argv[])
 
 	/* build the server's Internet address */
 	bzero(&serveraddr,sizeof(serveraddr));               //zero the struct
-	serveraddr.sin_family = AF_INET;                 //address family
+	serveraddr.sin_family = AF_INET;                
 	serveraddr.sin_port = htons(atoi(argv[2]));      //sets port to network byte order
 	serveraddr.sin_addr.s_addr = inet_addr(argv[1]); //sets remote IP address
 
-	server_hp = gethostbyname(argv[1]);					 // Return information about host in argv[1]
+	server_hp = gethostbyname(argv[1]);					 
 	if(server_hp < 0){
 			perror("Host Unknown");
 	}
@@ -90,7 +89,7 @@ int main (int argc, char * argv[])
 
 	while(1){
 		
-		timeout.tv_sec = 0;
+		timeout.tv_sec = 0; //Timeout for ack 
 		timeout.tv_usec = 0;
 		setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
@@ -100,11 +99,11 @@ int main (int argc, char * argv[])
 		printf("Enter the command to be performed and type it: get [filename], put[filename], delete[filename], ls, exit\n");
 		gets(cmd);	
 		nbytes = sendto(sockfd, cmd, sizeof(cmd) , 0, (struct sockaddr *)&serveraddr, serverlen);
-		name_cmd = strdup(cmd);
+		name_cmd = strdup(cmd); //Splitting string
 		strtok(name_cmd, " ");
 		fname = strtok(NULL, " ");
 		printf("Filename: %s\n", fname);
-		if(!strcmp(name_cmd, "get"))
+		if(strcmp(name_cmd, "get") == 0)
 		   {
 			printf("Get the file: %s from the server.\n", fname);
 			int packet_id=1; 	
@@ -118,36 +117,35 @@ int main (int argc, char * argv[])
 			{
 				bzero(s->data_descp, sizeof(s->data_descp));
 				nbytes = recvfrom(sockfd, (Packet_Details*) s, sizeof(Packet_Details), 0, (struct sockaddr *) &remote, &serverlen);
-
 				printf("Packet Size from the server: %d \n",nbytes);
-
 				encode(s->data_descp, s->packet_length, key);
-				if(s->pckt_index != packet_id){
+				if(s->pckt_index != packet_id) //if the packet index doesn't match the index that is expected then send again
+				{
 					a->pckt_ack=s->pckt_index;
 					nbytes = sendto(sockfd, (Packet_Details*)a, sizeof(Packet_Details), 0, (struct sockaddr *)&serveraddr,serverlen);
-					printf("Packet Size being sent to server: %d and ACK sent: %d\n",nbytes, a->pckt_ack);
+					printf("Packet Size  that is being sent to server: %d and ACK sent: %d\n",nbytes, a->pckt_ack);
 					
 				}
-				else if(s->pckt_index == packet_id){
-					printf("Writing to the file\n" );
+				else if(s->pckt_index == packet_id){  //if they are equal then write to file
+					printf(" Now write to the file\n" );
 					FILE *fptr;
 					fptr = fopen(fname,"ab");
 					fwrite(s->data_descp, s->packet_length, 1, fptr);
 					bzero(a->data_descp, sizeof(a->data_descp));
 					fclose(fptr);
-
 					a->pckt_ack=packet_id;
 					nbytes = sendto(sockfd, (Packet_Details*) a, sizeof(Packet_Details), 0, (struct sockaddr *)&serveraddr, serverlen);
 					printf("Packet Size being sent to server: %d and ACK sent: %d\n",nbytes, a->pckt_ack);
 					packet_id++;
 	
 				}
-				if(s->packet_length != BUFSIZE){
+				if(s->packet_length != BUFSIZE) 
+				{
 					break;
 				}
 			} 
 			
-			memset(a, 0, sizeof(Packet_Details));
+			memset(a, 0, sizeof(Packet_Details)); //Free memory
 			memset(s, 0, sizeof(Packet_Details));
 		}
 
@@ -166,15 +164,15 @@ int main (int argc, char * argv[])
 			fptr = fopen(fname, "rb");
 			if(fptr != NULL)
 			{
-				char msg[] = "Success";
+				char msg[] = "Filefound";
 				nbytes = sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&serveraddr, serverlen);
 				
-				}
+			}
 
 			else if(fptr == NULL)
 			{
 				perror("File does nor exist or Error opening file\n");
-				char msg[] = "Error";
+				char msg[] = "Fileerror";
 				nbytes = sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&serveraddr, serverlen);
 				continue;
 				
@@ -185,14 +183,13 @@ int main (int argc, char * argv[])
 				
 				read_length = fread(a->data_descp, 1, BUFSIZE , fptr);
 				a->packet_length = read_length;
-
 				encode(a->data_descp, a->packet_length, key); //encrypting data to be sent on the server
 				nbytes = sendto(sockfd, (Packet_Details*) a, sizeof(Packet_Details), 0, (struct sockaddr *)&serveraddr, serverlen);
 				printf("Packet Size sent to the server: %d\n",nbytes);
-
 				bzero(s->data_descp, sizeof(s->data_descp));
 				nbytes = recvfrom(sockfd, (Packet_Details*) s, sizeof(Packet_Details), 0, (struct sockaddr *)&remote, &serverlen);
-				if(nbytes < 0){
+				if(nbytes < 0) //condition for timeout
+				{
 					printf("Timeout has occurred\n");
 					fseek(fptr, (-1)*read_length, SEEK_CUR);
 					continue;
@@ -200,19 +197,20 @@ int main (int argc, char * argv[])
 				else
 				{
 					printf("Packet Size from server: %d and ack_index from server: %d \n", nbytes, s->pckt_ack);
-					if(s->pckt_ack == a->pckt_index){
-						a->pckt_index++;
-				}
+					if(s->pckt_ack == a->pckt_index){  
+						a->pckt_index++; //incremeting the index 
+				 }
 				else{
-						fseek(fptr, (-1)*read_length, SEEK_CUR);
+						fseek(fptr, (-1)*read_length, SEEK_CUR); //Time out has ocurred
 					
 				}
 				}
-				if(a->packet_length != BUFSIZE){
+				if(a->packet_length != BUFSIZE)
+				{
 					break;
 				}
 			}
-			memset(a, 0, sizeof(Packet_Details));
+			memset(a, 0, sizeof(Packet_Details)); //free the memory
 			memset(s, 0, sizeof(Packet_Details));
 		}
 		else if(strcmp("ls", name_cmd) == 0)
