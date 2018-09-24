49db7f430db8ed674ef4a7e65bd11ef5e6b0cb02
Sanika Dongre <sado9543@colorado.edu>
2018-09-23 18:14:58 -0600

    Update server.c

--- netsys_t/server/server.c
+++ netsys_t/server/server.c
@@ -1,19 +1,24 @@
 
-#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdio.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <sys/time.h>
-#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
 #include <memory.h>
 #include <string.h>
+#include <stdint.h>
+#include <sys/time.h>
 #include <dirent.h>
+
 #define BUFSIZE (1024)
 
 void data_encryption(char *buffer, int data_len, char key1[], char key2[]);
@@ -26,6 +31,14 @@ typedef struct{
 	char data_buff[BUFSIZE];				// to store packet data
 	int len_data;											// to store packet data length
 }struct_pckt;
+
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
 
 /***** DATA DECRYPTION FUNCTION *****/
 void data_decryption(char *buffer, int data_len, char key1[], char key2[]){
@@ -51,17 +64,21 @@ void data_encryption(char *buffer, int data_len, char key1[], char key2[]){
 int main (int argc, char * argv[] )
 {
 			//to set stdout to not buffer printf -> stackoverflow.com
-	int sock;                           	// this will be our socket
-	struct sockaddr_in sin, remote;     	//"Internet socket address structure"
-	unsigned int remote_length;         	// length of the sockaddr_in structure
+	int sockfd, portno, clientlen, optval, n, bytestot =0;                           	// this will be our socket
+	struct sockaddr_in sin, clientaddr;     	//"Internet socket address structure
 	int nbytes;                        		// number of bytes to be sent or received
-	char buffer[BUFSIZE];             	// buffer for storing the data to be sent/recieved
+	char buffer[BUFSIZE], buf[BUFSIZE], hash_buf[BUFSIZE], recv_buf[BUFSIZE];             	// buffer for storing the data to be sent/recieved
 	char *cname;													// to extract the command from user input at client
 	char *filename;												// to store the filename(in any) from user input at client
 	struct_pckt* c_pckt = malloc(sizeof(struct_pckt));// defining packet structure for client packet
 	struct_pckt* s_pckt = malloc(sizeof(struct_pckt));// defining packet structure for server packet
-	FILE *fp;															// declaring file pointer to perform file read/write
-	int read_length;											// to store the length of the read data
+	FILE *fptr;															// declaring file pointer to perform file read/write
+	int read_length;	
+	uint8_t cmd_out_exit, val[BUFSIZE] = "The server is exiting", cmd[70], fname[70], check = 1;
+  	bzero(cmd, sizeof(cmd));
+  	bzero(fname, sizeof(fname));
+  	bzero(val, sizeof(val));
+  	uint8_t msg_conf[] = " ";// to store the length of the read data
 	struct timeval timeout;								// defining timeout structure
 	char key1[39] = "MyMethodOfEncrptingTheDataIIntendToSend"; // Data encryption key-1
 	char key2[59] = "ThisIsMySecondKeyForMyMethodOfEncrptingTheDataIIntendToSend";// Data encryption key-2
@@ -83,36 +100,28 @@ to start the server *****/
 	sin.sin_family = AF_INET;                   //address family
 	sin.sin_port = htons(atoi(argv[1]));        //htons() sets the port # to network byte order
 	sin.sin_addr.s_addr = INADDR_ANY;           //supplies the IP address of the local machine
-
-
-	/***** Causes the system to create a generic socket of type UDP (datagram)*****/
-	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
 	{
 		printf("unable to create socket");
 	}
-
-	/******************
-	  Once we've created a socket, we must bind that socket to the
-	  local address and port we've supplied in the sockaddr_in struct
-	 ******************/
-	if (bind(sock, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+	if (bind(sockfd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
 	{
 		printf("unable to bind socket\n");
 	}
 
-	remote_length = sizeof(struct sockaddr_in);
+	clientlen = sizeof(struct sockaddr_in);
 
 	//waits for an incoming message
 	while(1){
 		/* Initializing timeout struct */
 		timeout.tv_sec = 0;
 		timeout.tv_usec = 0;
-		setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+		setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
 		bzero(buffer, sizeof(buffer));
 
 		/* Recieving the command from the client */
-		nbytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&remote, &remote_length);
+		nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientaddr, &clientlen);
 
 		/***** Split the command from the user to get the command and filename if any
 		 Refernce: http://www.geeksforgeeks.org/how-to-split-a-string-in-cc-python-and-java/
@@ -131,41 +140,41 @@ to start the server *****/
 			/* Setting Timeout */
 			timeout.tv_sec = 0;
 			timeout.tv_usec = 300000;
-			setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+			setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
 
 			s_pckt->pckt_index = 1;	// Initializing the server packet index with 1
 			c_pckt->pckt_ack = 0;		// Initializing the packet acknowledgement with 0
 
-			FILE *fp;
-			fp = fopen(filename, "rb");
-			if(fp == NULL){
+			FILE *fptr;
+			fptr = fopen(filename, "rb");
+			if(fptr == NULL){
 				perror("File does nor exist or Error opening file \n");
 				char msg[] = "Error";
-				nbytes = sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *)&remote, remote_length);
+				nbytes = sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&clientaddr, clientlen);
 				continue;
 			}
 			else{
 				char msg[] = "Success";
-				nbytes = sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *)&remote, remote_length);
+				nbytes = sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&clientaddr, clientlen);
 			}
 			//Starting the loop for continous sending of data to the server
 			do{
 				bzero(s_pckt->data_buff, sizeof(s_pckt->data_buff));
-				read_length = fread(s_pckt->data_buff, 1, BUFSIZE , fp);
+				read_length = fread(s_pckt->data_buff, 1, BUFSIZE , fptr);
 				s_pckt->len_data = read_length;
 				printf("Read length%d\n", read_length );
 
 				data_encryption(s_pckt->data_buff, s_pckt->len_data, key1, key2);//Encrypting data to be sent to the client
-				nbytes = sendto(sock, (struct_pckt*) s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&remote, remote_length);
+				nbytes = sendto(sockfd, (struct_pckt*) s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&clientaddr, clientlen);
 				printf("Packet Size send to client: %d\n",nbytes);
 
 				bzero(s_pckt->data_buff, sizeof(s_pckt->data_buff));
-				nbytes = recvfrom(sock, (struct_pckt*) c_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&remote, &remote_length);
+				nbytes = recvfrom(sockfd, (struct_pckt*) c_pckt, sizeof(struct_pckt), 0, (struct sockaddr *)&clientaddr, &clientlen);
 
 				//Packet Authentication
 				if(nbytes<0){
 					printf("---------------------Timeout--------------------------\n");
-					fseek(fp, (-1)*read_length, SEEK_CUR);
+					fseek(fptr, (-1)*read_length, SEEK_CUR);
 					continue;
 				}
 				printf("Packet Size from client: %d and client ack_index: %d \n",nbytes, c_pckt->pckt_ack);
@@ -176,7 +185,7 @@ to start the server *****/
 				}
 				//retaining Server packet index if the expected acknowledgement is not recieved
 				else{
-					fseek(fp, (-1)*read_length, SEEK_CUR);
+					fseek(fptr, (-1)*read_length, SEEK_CUR);
 				}
 				if(read_length != BUFSIZE){
 					break;
@@ -200,21 +209,21 @@ to start the server *****/
 			s_pckt->pckt_ack = 1;//Initializing packet structure acknowledgement
 
 			bzero(buffer, sizeof(buffer));
-			nbytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &remote, &remote_length);
+			nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &clientaddr, &clientlen);
 
 			if(!strcmp(buffer, "Error")){
 				continue;
 			}
-			FILE *fp;
-			fp = fopen(filename,"ab");
-			if(fp == NULL){
+			FILE *fptr;
+			fptr = fopen(filename,"ab");
+			if(fptr == NULL){
 				perror("Error opening file \n");
 			}
 
 			/* Loop for continous receiving of valid packets */
 			do{
 				bzero(c_pckt->data_buff, sizeof(c_pckt->data_buff));
-				nbytes = recvfrom(sock, (struct_pckt*) c_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &remote, &remote_length);
+				nbytes = recvfrom(sockfd, (struct_pckt*) c_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &clientaddr, &clientlen);
 				printf("Packet Size from the client: %d \n",nbytes);
 
 				data_decryption(c_pckt->data_buff, c_pckt->len_data, key1, key2);//decrypting data sent by the client
@@ -222,24 +231,23 @@ to start the server *****/
 				/* Packet Validation */
 				if(c_pckt->pckt_index == exp_index){
 					printf("Writing to the file \n" );
-					fwrite(c_pckt->data_buff, c_pckt->len_data, 1, fp);
+					fwrite(c_pckt->data_buff, c_pckt->len_data, 1, fptr);
 					bzero(c_pckt->data_buff, sizeof(c_pckt->data_buff));
 					s_pckt->pckt_ack=exp_index;
 					//Sending Acknowledgement if the expected packet is recieved
-					nbytes = sendto(sock, (struct_pckt*) s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &remote, remote_length);
+					nbytes = sendto(sockfd, (struct_pckt*) s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &clientaddr, clientlen);
 					printf("Packet Size being sent to client: %d and ACK from server: %d\n",nbytes, s_pckt->pckt_ack);
 					exp_index++;
 				}
 				else{
 					s_pckt->pckt_ack=c_pckt->pckt_index;
 					//Sending Acknowledgement if the expected packet not received
-					nbytes = sendto(sock, (struct_pckt*)s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &remote, remote_length);
-				}
+					nbytes = sendto(sockfd, (struct_pckt*)s_pckt, sizeof(struct_pckt), 0, (struct sockaddr *) &clientaddr, clientlen);
 				if(c_pckt->len_data != BUFSIZE){
 					break;
 				}
 			}while(1); // check for remaining data to be recieved
-			fclose(fp);
+			fclose(fptr);
 
 			// Clearing the memory for the packet structures
 			memset(c_pckt, 0, sizeof(struct_pckt));
@@ -249,33 +257,53 @@ to start the server *****/
 		/***** Condition: To delete a file from the server (requested by the client)
 		Given a valid file name by the user, which is available
 		at the server *****/
-		else if(!strcmp(cname,"delete")){
-			if(remove(filename)){
-				char msg[] = "unable to delete file";
-				// Sending acknowledgement for error in deleting the file.
-				nbytes = sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *) &remote, remote_length);
-				perror("Error");
+		else if(!strcmp(cname,"delete"))
+		{
+			FILE *f;
+			bytestot = recvfrom(sockfd, filename, (BUFSIZE), 0, (struct sockaddr*)&clientaddr, &(clientlen));
+			printf("The file name is %s\n", filename);
+			f = fopen(filename,"r");			
+			if(f != NULL)
+			{
+				file_del = remove(filename);
+				if(file_del != 0)
+				{
+					perror("Error");
+					printf("Enter a valid file name\n");
+				}
+				if(file_del == 0)
+				{
+					printf("The file is deleted\n");
+				}
 			}
-			else{
-				//Sending acknowledgement for deleting the file.
-				char msg[] = "Deleted File";
-				nbytes = sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *) &remote, remote_length);
+			if(f == NULL)
+			{
+			  	perror("Error");
+				printf("The file is not found and can't be deleted\n");
 			}
 		}
 
-		/***** Condition: Client's request to list all file from the server
-		Thus, sending the file-names in the directory to the client
-		Reference: http://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/ *****/
+		
 		else if(!strcmp(cname,"ls")){
-			DIR *current_dir;
-			struct dirent *struct_dir;
-
-			current_dir = opendir(".");
-			if(current_dir == NULL){
-				char msg[] = "Unable to read Directory";
-				/* sending error if unable to read the directory */
-				nbytes = sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *) &remote, remote_length);
+			DIR *direct_ls;
+			struct dirent *dir_ls;
+			direct_ls = opendir(".");
+			if(direct_ls == NULL)
+			{
+				strcpy(msg_conf, "The drirectory can't be read");
+				bytestot = sendto(sockfd, msg_conf, sizeof(msg_conf), 0, (struct sockaddr*)&clientaddr, clientlen);
 				perror("Error");
+			}
+			else
+			{
+				bzero(fname, BUFSIZE);
+				while((dir_ls == readdir(direct_ls)) != NULL)
+				{
+					strcat(fname, dir_ls->d_name);
+					strcat(fname, "\n");
+				}
+				bytestot = sendto(sockfd, fname, sizeof(fname), 0, (struct sockaddr*)&clientaddr, clientlen);
+			}
 			}
 			else{
 				bzero(buffer, sizeof(buffer));
@@ -285,23 +313,37 @@ to start the server *****/
 					strcat(buffer, "\n");
 				}
 			/* Sending the file-names to the client*/
-			nbytes = sendto(sock, buffer, sizeof(buffer) , 0, (struct sockaddr *)&remote, remote_length);
+			nbytes = sendto(sockfd, buffer, sizeof(buffer) , 0, (struct sockaddr *)&clientaddr, clientlen);
 			}
 		}
-		/***** Condition: Client's request to the server
-		to release the connection *****/
-		else if(!strcmp(cname,"exit")){
-			char msg[] = "Connection Released by Server";
-			nbytes = sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *) &remote, remote_length);
-			close(sock);
+		
+		else if(!strcmp(cname,"exit"))
+		{
+			bzero(buf, sizeof(buf));
+			strcat(filename, "Exit");
+			printf(" Exit buffer: %s\n", buf);
+			bytestot = sendto(sockfd, buf, BUFSIZE, 0, (struct sockaddr*)&clientaddr, clientlen);
 			exit(0);
 		}
-
-		else{
-			printf("Unsupported Command \n");
+		else if(strcmp("md5", cname) == 0)
+		{
+			strcpy(hash_buf, "md5sum");
+			printf("To get the hash value of the file: %s\n", filename);
+			strncat(hash_buf,filename,strlen(filename));
+			printf("**************************\n");
+			system(hash_buf);
+			printf("***************************\n");
+		}
+		else
+		{
+			printf("The command in not correct\n");
+			printf("Enter a valid command\n");
 		}
 
 		bzero(buffer, sizeof(buffer));
+		bzero(fname, sizeof(fname));
+		bzero(val,sizeof(val));
+
 		}
 
 }
